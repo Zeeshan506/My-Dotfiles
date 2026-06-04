7e04a426a145e43246b682b2ef89ecc91931d68b
Zeeshan506 <zeeshanazhar506@gmail.com>
2026-06-04 18:32:34 +0500

    Added Rofi And Updated Power Menu Script

--- .config/waybar/power_menu.sh
+++ .config/waybar/power_menu.sh
@@ -10,8 +10,8 @@ shutdown="⏻  Shutdown"
 # Format the options with newlines
 options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
 
-# Pipe the options into hyprlauncher
-chosen=$(echo -e "$options" | hyprlauncher --dmenu)
+# Pipe the options into rofi using the -dmenu flag
+chosen=$(echo -e "$options" | rofi -dmenu -i -p "Power")
 
 # Execute the corresponding command based on the selection
 case $chosen in
